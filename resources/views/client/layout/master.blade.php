d9ebb6169547e985bcbd393b481ca0164b8a7197
unknown <verdiyevarana@gmail.com>
2025-11-14 13:56:27 +0400

    qachmalar duzeldildi responsivlikde mobil nomre ve soical medida admin panele baglandi

--- resources/views/client/layout/master.blade.php
+++ resources/views/client/layout/master.blade.php
@@ -1,3 +1,10 @@
+@php
+use App\Models\Setting;
+
+$settings = Setting::firstOrDefault();
+
+@endphp
+
 <!DOCTYPE html>
 <html lang="az">
 
@@ -42,19 +49,30 @@
       <ul class="mobile-nav__contact list-unstyled">
         <li>
           <i class="fa fa-envelope"></i>
-          <a href="mailto:needhelp@packageName__.com">needhelp@envens.com</a>
+          <a href="mailto:{{ $settings->email ?? 'info@example.com' }}">{{ $settings->email ?? 'info@example.com' }}</a>
         </li>
-        <li>
+        <li> 
           <i class="fa fa-phone-alt"></i>
-          <a href="tel:666-888-0000">666 888 0000</a>
+          <a href="tel:{{ $settings->phone ?? '+994 50 000 00 00' }}">{{ $settings->phone ?? '+994 50 000 00 00' }}</a>
         </li>
       </ul><!-- /.mobile-nav__contact -->
       <div class="mobile-nav__top">
         <div class="mobile-nav__social">
-          <a href="#" class="fab fa-twitter"></a>
-          <a href="#" class="fab fa-facebook-square"></a>
-          <a href="#" class="fab fa-pinterest-p"></a>
-          <a href="#" class="fab fa-instagram"></a>
+          @if($settings->facebook_link)
+                        <a href="{{ $settings->facebook_link }}" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        @endif
+                        @if($settings->twitter_link)
+                        <a href="{{ $settings->twitter_link }}" target="_blank"><i class="fab fa-twitter"></i></a>
+                        @endif
+                        @if($settings->linkedin_link)
+                        <a href="{{ $settings->linkedin_link }}" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                        @endif
+                        @if($settings->instagram_link)
+                        <a href="{{ $settings->instagram_link }}" target="_blank"><i class="fab fa-instagram"></i></a>
+                        @endif
+                        @if($settings->youtube_link)
+                        <a href="{{ $settings->youtube_link }}" target="_blank"><i class="fab fa-youtube"></i></a>
+                        @endif
         </div><!-- /.mobile-nav__social -->
       </div><!-- /.mobile-nav__top -->
 
